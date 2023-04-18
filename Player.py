64ab607cdbdc9c056851c96595bca4f2f46aba86
ChristianFredrik69 <cjohnsen41@gmail.com>
2023-04-18 17:59:59 +0200

    Player-klasse

--- Player.py
+++ Player.py
@@ -5,15 +5,19 @@ class Player():
     def __init__(self, player_id):
         self.hand = []
         self.player_id = player_id
+        self.stack = []
 
-
-    def draw_card(self):
-        self.hand.append(cardDeck.drawCard())
-
-    def play_card(self, cardIndex):
-        self.hand.pop(cardIndex)
+    def get_player_id(self):
+        return self.player_id
     
+
+    # def draw_card(self):
+    #     self.hand.append(cardDeck.drawCard())
+
+    # def play_card(self, cardIndex):
+    #     self.hand.pop(cardIndex)
     
+
 
     
 
